37a446aacd9421c8fc1e3cd24d800d8cc0cc2321
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-11-09 01:28:39 -0300

    checagem pra mac

--- fetch_tsv.sh
+++ fetch_tsv.sh
@@ -4,7 +4,10 @@
 
 TSV_DIR="$PWD/tsv"
 BASE_URL="https://datasets.imdbws.com/"
-TSV_FILES='title.basics.tsv title.ratings.tsv'
+TSV_FILES='title.basics.tsv title.ratings.tsv title.principals.tsv'
+
+[ $(uname) == Darwin ]
+IS_MAC=$?
 
 for tsv_file in $TSV_FILES
 do
@@ -14,7 +17,13 @@ do
     gzip -d $tsv_file.gz
 
     # replace invalid characters
-    sed -i -e 's/\\N//g' -e 's/\t/|/g' -e "s/\"//g" $tsv_file
+    if [ $IS_MAC -eq 0 ]; then
+        # mac os needs a '' after -i
+        sed -i '' -e "s/\\\\N//g" -e 's/\t/|/g' -e "s/\"//g" $tsv_file
+    else
+        # linux and wsl
+        sed -i -e "s/\\\\N//g" -e 's/\t/|/g' -e "s/\"//g" $tsv_file
+    fi;
 
     popd > /dev/null
 done
