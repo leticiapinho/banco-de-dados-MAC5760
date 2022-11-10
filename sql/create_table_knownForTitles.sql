99894eb2e1b851fd100d3a81b75648d599137028
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-11-10 19:52:49 -0300

    voltando com os not null

--- sql/create_table_knownForTitles.sql
+++ sql/create_table_knownForTitles.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS knownForTitles
 (
 	nconst varchar(50) NOT NULL,
-	tconst varchar(50)
+	tconst varchar(50) NOT NULL
 );
 
 \copy knownForTitles (nconst, tconst) FROM 'SQL_FILE' DELIMITER E'|' CSV HEADER;
--- sql/create_table_primaryProfession.sql
+++ sql/create_table_primaryProfession.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS primaryProfession
 (
 	nconst varchar(50) NOT NULL,
-	idprof varchar(50)
+	idprof varchar(50) NOT NULL
 );
 
 \copy primaryProfession (nconst, idprof) FROM 'SQL_FILE' DELIMITER E'|' CSV HEADER;
