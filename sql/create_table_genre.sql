03860c68802d4fd04c0bf2a0510485e594aa4121
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-11-12 19:10:21 -0300

    Revert "tirando registros inconsistentes"
    This reverts commit 18e86cd5aa376701455ba5f61e82195b9dd3e2b1.

--- sql/create_table_genre.sql
+++ sql/create_table_genre.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS genre
 (
     idgen varchar (50) NOT NULL,
-    tconst varchar (50)
+    tconst varchar (50),
+    FOREIGN KEY (tconst) REFERENCES titleBasics(tconst)
 );
 
 \copy genre (idgen, tconst)  FROM 'SQL_FILE' DELIMITER E'|' CSV HEADER;
-
-DELETE FROM genre WHERE tconst NOT IN (SELECT tconst FROM titleBasics);
--- sql/create_table_knownForTitles.sql
+++ sql/create_table_knownForTitles.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS knownForTitles
 (
 	nconst varchar(50) NOT NULL,
-	tconst varchar(50) NOT NULL
+	tconst varchar(50) NOT NULL,
+	FOREIGN KEY (tconst) REFERENCES titleBasics(tconst)
 );
 
 \copy knownForTitles (nconst, tconst) FROM 'SQL_FILE' DELIMITER E'|' CSV HEADER;
-
-DELETE FROM knownForTitles WHERE tconst NOT IN (SELECT tconst FROM titleBasics);
