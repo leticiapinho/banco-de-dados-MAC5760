662d36946c4b01b2d21aa87bfd8d89e6cacdf735
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-11-14 16:54:02 -0300

    alterações no schema do banco

--- sql/create_table_primaryProfession.sql
+++ sql/create_table_primaryProfession.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS primaryProfession
 (
 	nconst varchar(50) NOT NULL,
         idprof varchar(50) NOT NULL,
-	FOREIGN KEY (nconst) REFERENCES nameBasics(nconst)
+	FOREIGN KEY (nconst) REFERENCES nameBasics(nconst) ON DELETE CASCADE
 );
 
 \copy primaryProfession (nconst, idprof) FROM 'SQL_FILE' DELIMITER E'|' CSV HEADER;
